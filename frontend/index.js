744a90cd72c9f379dc3ac7cffb32f360158e6750
Paul Maloney <paul@paulmaloney.com>
2024-04-11 20:40:36 -0700

    completed project

--- frontend/index.js
+++ frontend/index.js
@@ -3,7 +3,8 @@ function moduleProject2() {
   // 👇 WORK WORK BELOW THIS LINE 👇
   let startTime = new Date().getTime() // Record start time
 
-  function getTimeElapsed() { // To be used at end of game to get elapsed time
+  // To be used at end of game to get elapsed time
+  function getTimeElapsed() { 
     let currentTime = new Date().getTime()
     return currentTime - startTime
   }
@@ -37,6 +38,9 @@ function moduleProject2() {
       row.appendChild(square)
       square.addEventListener('click', () => {
         // 👉 TASK 2 - Use a click handler to target a square 👈
+        const targetedSquares = document.querySelectorAll(".targeted")
+        targetedSquares.forEach(targetedSquare => {targetedSquare.classList.remove("targeted")})
+        square.classList.add("targeted")
       })
     }
   }
@@ -63,15 +67,85 @@ function moduleProject2() {
     allSquares[randomInt].appendChild(mosquito)
   })
 
+
+
+
+  // let keys = { // To easily check `event.key` on keyboard events
+  //   space: ' ',
+  //   up: 'ArrowUp',
+  //   right: 'ArrowRight',
+  //   down: 'ArrowDown',
+  //   left: 'ArrowLeft',
+  // }
+  function restartGame() {
+    window.location.reload()
+  }
+
   document.addEventListener('keydown', evt => {
     // 👉 TASK 3 - Use the arrow keys to highlight a new square 👈
+    
+    const currentSq = document.querySelector(".targeted")
+    const currentParent = currentSq.parentElement //row is parent of square; indexOf this is the col
+    const allColumnsArr = Array.from(currentParent.children)
+    const colIndex = allColumnsArr.indexOf(currentSq)
+    const currentGrandParent = currentParent.parentElement //grid is the parent of row; indexOf this is the row
+    const allRowsArr = Array.from(currentGrandParent.children)
+    const rowIndex = allRowsArr.indexOf(currentParent)
+    
+    if (evt.key === keys.up) {
+      if (rowIndex>0) {
+        document.querySelector(`.row:nth-child(${rowIndex})`).children[colIndex].classList.add('targeted')
+        document.querySelector(`.row:nth-child(${rowIndex+1})`).children[colIndex].classList.remove('targeted')
+      }
+    }
+
+    if (evt.key === keys.down) {
+      if (rowIndex<4) {
+        document.querySelector(`.row:nth-child(${rowIndex+2})`).children[colIndex].classList.add('targeted')
+        document.querySelector(`.row:nth-child(${rowIndex+1})`).children[colIndex].classList.remove('targeted')
+      }
+    }
+
+    if (evt.key === keys.left) { 
+      if (colIndex>0) {
+        document.querySelector(`.row:nth-child(${rowIndex+1})`).children[colIndex-1].classList.add('targeted')
+        document.querySelector(`.row:nth-child(${rowIndex+1})`).children[colIndex].classList.remove('targeted')
+      }
+    }
+
+    if (evt.key === keys.right) {
+      if (colIndex<4) {
+        document.querySelector(`.row:nth-child(${rowIndex+1})`).children[colIndex+1].classList.add('targeted')
+        document.querySelector(`.row:nth-child(${rowIndex+1})`).children[colIndex].classList.remove('targeted')
+      }
+    }
+
+    
 
     // 👉 TASK 4 - Use the space bar to exterminate a mosquito 👈
-
-    // 👉 TASK 5 - End the game 👈
+    if (evt.key === keys.space) {
+      const targetedSq = document.querySelector(".targeted")
+      const imgTarget = document.querySelector(".targeted img")
+      if (imgTarget) {
+        targetedSq.style.backgroundColor = "red"
+        imgTarget.setAttribute("data-status", "dead")
+      }
+      // 👉 TASK 5 - End the game 👈
+      const aliveMosquitoes = document.querySelectorAll('img[data-status="alive"]')
+      if (aliveMosquitoes.length===0) {
+        const gameTime = getTimeElapsed()
+        document.querySelector("p.info").textContent = `Extermination completed in ${gameTime} seconds!`
+        const header2 = document.querySelector("h2")
+        const restartButton = document.createElement("button")
+        restartButton.textContent = "Restart"
+        header2.appendChild(restartButton)
+        restartButton.addEventListener("click", restartGame)
+      }
+      
+    }
   })
-  // 👆 WORK WORK ABOVE THIS LINE 👆
 }
+  // 👆 WORK WORK ABOVE THIS LINE 👆
 
 // ❗ DO NOT MODIFY THE CODE BELOW
 // ❗ DO NOT MODIFY THE CODE BELOW
